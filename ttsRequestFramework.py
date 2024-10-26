64a5fb3798beaa54d7d01e9c2f4e27da51f181a4
Nicholas C <nicholaslching@gmail.com>
2024-10-26 14:32:56 -0400

    Updated Gemini and TTS Frameworks

--- ttsRequestFramework.py
+++ ttsRequestFramework.py
@@ -2,37 +2,45 @@ import os
 import requests
 from dotenv import load_dotenv, dotenv_values
 from playsound import playsound
-load_dotenv() 
 
-# Replace with your actual API key
-api_key = os.environ["TTS_API_KEY"]
+class ttsRequestFramework:
+    
+    api_key = None
+    voice_id = None
+    url = None
+    stability = None
+    similarityBoost = None
+    
+    def convertTTS(self, text):
+        payload = {
+            "text": text,
+            "voice_settings": {
+                "stability": 0.5,               # CNG to VAR
+                "similarity_boost": 0.6         # CNG to VAR
+            }
+        }
+        headers = {
+            "xi-api-key": ttsRequestFramework.api_key,
+            "Content-Type": "application/json"
+        }
 
-# Select the voice (replace with your desired voice ID)
-voice_id = "78pDRD8cI8CahmrAekML"
+        response = requests.request("POST", ttsRequestFramework.url, json=payload, headers=headers)
 
-url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+        # Check for success
+        if response.status_code == 200:
+            # Save the audio file
+            with open("output.mp3", "wb") as file:
+                file.write(response.content)
+            playsound('output.mp3')
+            print("Audio generated successfully!")
+        else:
+            print("Error:", response.status_code)
+            print(response.text)
 
-payload = {
-    "text": "Oh my, a data breach? Now that sounds scary. I don't know anything about computers, but my grandson, he's a whiz with them! I'll give him a call and see what he says.",
-    "voice_settings": {
-        "stability": 0.5,
-        "similarity_boost": 0.6
-    }
-}
-headers = {
-    "xi-api-key": api_key,
-    "Content-Type": "application/json"
-}
-
-response = requests.request("POST", url, json=payload, headers=headers)
-
-# Check for success
-if response.status_code == 200:
-    # Save the audio file
-    with open("output.mp3", "wb") as file:
-        file.write(response.content)
-    playsound('output.mp3')
-    print("Audio generated successfully!")
-else:
-    print("Error:", response.status_code)
-    print(response.text)
+    def __init__(self, stability, similarityBoost):
+        load_dotenv() 
+        ttsRequestFramework.api_key = os.environ["TTS_API_KEY"]
+        ttsRequestFramework.voice_id = "78pDRD8cI8CahmrAekML"
+        ttsRequestFramework.url = f"https://api.elevenlabs.io/v1/text-to-speech/{ttsRequestFramework.voice_id}"
+        ttsRequestFramework.stability = stability
+        ttsRequestFramework.similarityBoost = similarityBoost
