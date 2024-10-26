9c6d52ce8cd5be7efc948c92e28ad92ddd84f11c
Nicholas C <nicholaslching@gmail.com>
2024-10-26 14:11:44 -0400

    Updated Gemini Request Framework

--- geminiRequestFramework.py
+++ geminiRequestFramework.py
@@ -1,19 +1,25 @@
 import os 
 import google.generativeai as genai
 import time
-
 from dotenv import load_dotenv, dotenv_values
 
-load_dotenv() 
-print(os.environ["API_KEY"])
+class geminiRequestFramework: 
+    
+    model = None
+    myfile = None
 
-genai.configure(api_key=os.environ["API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+    def uploadAudio(self):
+        start = time.time()
+        geminiRequestFramework.myfile = genai.upload_file("sampleAudio.mp3")
+        print(f"{geminiRequestFramework.myfile=}\n______________\nUploaded in {time.time() - start:.2f} seconds")
 
-start = time.time()
-myfile = genai.upload_file("sampleAudio.mp3")
-print(f"{myfile=}\n______________\nUploaded in {time.time() - start:.2f} seconds")
-
-start = time.time()
-response = model.generate_content([myfile, "Respond to this scam as a grandma would."])
-print(f"{response.text}\n______________\nExecuted in {time.time() - start:.2f} seconds")
+    def generateResponse(self):
+        start = time.time()
+        response = geminiRequestFramework.model.generate_content([geminiRequestFramework.myfile, "Respond to this scam as a grandma would."])
+        print(f"{response.text}\n______________\nExecuted in {time.time() - start:.2f} seconds")
+    
+    def __init__(self):
+        load_dotenv() 
+        print(os.environ["API_KEY"])
+        genai.configure(api_key=os.environ["API_KEY"])
+        geminiRequestFramework.model = genai.GenerativeModel("gemini-1.5-flash")
