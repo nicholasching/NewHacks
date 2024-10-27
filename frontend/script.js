717bdcb78620c05e3d9d7517c609929195856d86
KurtisL7 <kurtislin24@gmail.com>
2024-10-26 23:52:44 -0400

    made a dynamic input operator

--- frontend/script.js
+++ frontend/script.js
@@ -7,9 +7,11 @@ fetch('http://localhost:8080/get_data')
   })
   .then(data => {
     console.log('Data received:', data);
-    for (i = 0; i < 4; i++) {
-        console.log(data[i], data[i]["rank"]);
-    }
+            i = 0;
+            code_1 = console.log(data[i+1], data[i+1]["rank"]);
+            code_1 = console.log(data[i+2], data[i+2]["rank"]);
+            code_1 = console.log(data[i+3], data[i+3]["rank"]);
+    
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
